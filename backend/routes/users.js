c8460820f76c3a5dc3f256cde1a0fe94556982e5
Carlos Revilla <clro26.k@gmail.com>
2020-03-30 19:24:09 +0200

    modificada la ruta users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const UserController = 
-require('../controllers/UserController');
+const UserController = require('../controllers/UserController');
+const {authentication,isAdmin} = require('../middleware/authentication')
+
 router.post('/register',UserController.register);
 router.post('/login',UserController.login);
 router.get('/info',authentication, UserController.getInfo);
